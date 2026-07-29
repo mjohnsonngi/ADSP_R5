8e8bb507f03c9550005b4e368aba95963e53f75a
Matt Johnson <94560240+mjohnsonngi@users.noreply.github.com>
2026-07-29 12:56:53 -0500

    Delete docker/wxsJoint/wxsjointasqc directory

--- docker/wxsJoint/wxsjointasqc/scripts/splitintsdivide.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-$GATK SplitIntervals \
-    -R $REF_FASTA \
-    -L /scratch1/fs1/cruchagac/WXSref/20190812_GATK_38_googlebundle/resources_broad_hg38_v0_wgs_calling_regions.hg38.interval_list \
-    -O /tmp \
-    --scatter-count $NUM_INTERVALS \
-    --interval-merging-rule OVERLAPPING_ONLY \
-    --subdivision-mode INTERVAL_SUBDIVISION
