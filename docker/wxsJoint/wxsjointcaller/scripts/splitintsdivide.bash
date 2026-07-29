3419a0dc9d4ad7f206ca7998a050534415603b9c
Matt Johnson <94560240+mjohnsonngi@users.noreply.github.com>
2026-07-29 12:57:06 -0500

    Delete docker/wxsJoint/wxsjointcaller directory

--- docker/wxsJoint/wxsjointcaller/scripts/splitintsdivide.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-$GATK SplitIntervals \
-    -R $REF_FASTA \
-    -L /intlists/WGS.interval_list \
-    -O /tmp \
-    --scatter-count $NUM_INTERVALS \
-    --interval-merging-rule OVERLAPPING_ONLY \
-    --subdivision-mode INTERVAL_SUBDIVISION
